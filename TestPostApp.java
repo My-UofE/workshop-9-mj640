903b706b616e50ebece7cdd38e6d1bab8e538e7b
mj640 <mj640@exeter.ac.uk>
2026-03-13 15:11:56 +0000

    Task 2

---
+++ TestPostApp.java
@@ -0,0 +1,10 @@
+public class TestPostApp {
+    public static void main(String[] args) {
+
+    // create a post object
+    Post test = new Post("Alex Adam", "Help with JavaE", "Hi, could anyone help me I need to learn how to code in java!");
+    
+    // print the post
+    System.out.println(test.toString());
+    }
+}
